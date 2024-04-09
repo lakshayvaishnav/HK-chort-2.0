818f08f6ced95516c113b89a3e377a9861cd868c
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-04-09 21:53:48 +0530

    learned about monorepos

---
+++ week-16-monorepo/week-16-2/apps/web/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import { Admin } from "@repo/ui/admin";
+
+export default () => {
+  return (
+    <div>
+      hii from admin Page
+      <div>
+        <Admin />
+      </div>
+    </div>
+  );
+};
---
+++ week-16-monorepo/week-16-2/packages/ui/src/admin.tsx
@@ -0,0 +1,10 @@
+import { InputBox } from "./input-box";
+
+export function Admin() {
+  return (
+    <div>
+      admin page
+      <InputBox />
+    </div>
+  );
+}
