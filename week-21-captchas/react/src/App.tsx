8062facdbcc1cbed0cf6ec80452c6024be880a63
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-04-28 00:21:04 +0530

    debugged captcha failure error

--- week-21-captchas/react/src/App.tsx
+++ week-21-captchas/react/src/App.tsx
@@ -1,30 +1,37 @@
-import { Turnstile } from '@marsidev/react-turnstile'
+import { Turnstile } from "@marsidev/react-turnstile";
 
-import './App.css'
-import axios from 'axios'
-import { useState } from 'react'
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  const [token, setToken] = useState<string>("")
+  const [token, setToken] = useState<string>("");
 
   return (
     <>
-      <input placeholder='OTP'></input>
-      <input placeholder='New password'></input>
+      <input placeholder="OTP"></input>
+      <input placeholder="New password"></input>
 
-      <Turnstile onSuccess={(token:any) => {
-        setToken(token)
-      }} siteKey='0x4AAAAAAAXtEe2JIeAEUcjX' />
+      <Turnstile
+        onSuccess={(token: any) => {
+          setToken(token);
+        }}
+        siteKey="0x4AAAAAAAYfTkn68Q8gR_3Q"
+      />
 
-      <button onClick={() => {
-        axios.post("http://localhost:3000/reset-password", {
-          email: "harkirat@gmail.com",
-          otp: "123456",
-          token: token,
-        })
-      }}>Update password</button>
+      <button
+        onClick={() => {
+          axios.post("http://localhost:3000/reset-password", {
+            email: "harkirat@gmail.com",
+            otp: "123456",
+            token: token,
+          });
+        }}
+      >
+        Update password
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
