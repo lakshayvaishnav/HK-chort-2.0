bcf7165ce8b77b6bc3ed6bb8426383e050777ffc
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-05-18 19:15:39 +0530

    need to debug receiver and sender is looping

--- week-23-webRtc/backend/dist/index.js
+++ week-23-webRtc/backend/dist/index.js
@@ -2,45 +2,36 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-let senderSocker = null;
+let senderSocket = null;
 let receiverSocker = null;
 wss.on("connection", function connection(ws) {
     ws.on("error", console.error);
     ws.on("message", function message(data) {
         const message = JSON.parse(data);
-        // identify as sender
-        // identify as reciever
-        // create offer
-        // create answer
-        // add ice candidate
         if (message.type === "sender") {
-            senderSocker = ws;
-            console.log("sender set: ");
+            senderSocket = ws;
         }
         else if (message.type === "receiver") {
             receiverSocker = ws;
-            console.log("reciever set");
         }
         else if (message.type === "createOffer") {
-            if (ws !== senderSocker) {
+            if (ws !== senderSocket) {
                 return;
             }
-            console.log("offer recieved");
             receiverSocker === null || receiverSocker === void 0 ? void 0 : receiverSocker.send(JSON.stringify({ type: "createOffer", sdp: message.sdp }));
         }
         else if (message.type === "createAnswer") {
             if (ws !== receiverSocker) {
                 return;
             }
-            console.log("answer recieved...");
-            senderSocker === null || senderSocker === void 0 ? void 0 : senderSocker.send(JSON.stringify({ type: "createAnswer", sdp: message.sdp }));
+            senderSocket === null || senderSocket === void 0 ? void 0 : senderSocket.send(JSON.stringify({ type: "createAnswer", sdp: message.sdp }));
         }
         else if (message.type === "iceCandidate") {
-            if (ws === senderSocker) {
+            if (ws === senderSocket) {
                 receiverSocker === null || receiverSocker === void 0 ? void 0 : receiverSocker.send(JSON.stringify({ type: "iceCandidate", candidate: message.candidate }));
             }
             else if (ws === receiverSocker) {
-                senderSocker === null || senderSocker === void 0 ? void 0 : senderSocker.send(JSON.stringify({ type: "iceCandidate", candidate: message.candidate }));
+                JSON.stringify({ type: "iceCandidate", candidate: message.candidate });
             }
         }
     });
--- week-23-webRtc/frontend/src/App.tsx
+++ week-23-webRtc/frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Reciever from "./components/Reciever";
-import Sender from "./components/Sender";
+import { Receiver } from "./components/Receiver";
+import { Sender } from "./components/Sender";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/reciever" element={<Reciever />} />
+          <Route path="/reciever" element={<Receiver />} />
           <Route path="/Sender" element={<Sender />} />
         </Routes>
       </BrowserRouter>
