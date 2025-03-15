413386ab15973bc6a41969952a6a7b582167509c
Lakshay Vaishnav <thelakshayvaishnav@gmail.com>
2025-03-16 04:47:20 +0530

    added pubsub logic

--- week-31-scalable-sockets/chat-app/src/index.ts
+++ week-31-scalable-sockets/chat-app/src/index.ts
@@ -1,53 +1,76 @@
 import ws, { WebSocket, WebSocketServer } from "ws";
 import { createClient } from "redis";
 
-// single client can either publish or subscribe
-const client = createClient();
-client.connect();
+async function main() {
+  // single client can either publish or subscribe
+  const publishClient = createClient();
+  await publishClient.connect();
 
-const wss = new WebSocketServer({ port: 8080 });
+  const subscribeClient = createClient();
+  await subscribeClient.connect();
 
-const subscriptions: { [key: string]: { ws: WebSocket; rooms: string[] } } = {};
+  const wss = new WebSocketServer({ port: 8080 });
 
-setInterval(() => {
-  console.log(subscriptions);
-}, 5000);
+  const subscriptions: { [key: string]: { ws: WebSocket; rooms: string[] } } =
+    {};
 
-wss.on("connection", function connection(ws) {
-  const id = randomId();
-  subscriptions[id] = {
-    ws: ws,
-    rooms: [],
-  };
+  setInterval(() => {
+    console.log(subscriptions);
+  }, 5000);
 
-  ws.on("message", function message(data) {
-    // @ts-ignore
-    const parsedMessage = JSON.parse(data);
-    if (parsedMessage.type === "SUBSCRIBE") {
-      subscriptions[id].rooms.push(parsedMessage.room);
-    }
+  wss.on("connection", function connection(ws) {
+    const id = randomId();
+    subscriptions[id] = {
+      ws: ws,
+      rooms: [],
+    };
 
-    if (parsedMessage.type === "UNSUBSCRIBE") {
-      subscriptions[id].rooms = subscriptions[id].rooms.filter(
-        (x) => x !== parsedMessage.room
-      );
-    }
+    ws.on("message", function message(data) {
+      // @ts-ignore
+      const parsedMessage = JSON.parse(data);
+      if (parsedMessage.type === "SUBSCRIBE") {
+        subscriptions[id].rooms.push(parsedMessage.room);
 
-    if (parsedMessage.type === "sendMessage") {
-      const message = parsedMessage.message;
-      const roomId = parsedMessage.roomId;
-
-      Object.values(subscriptions).forEach(({ ws, rooms }) => {
-        if (rooms.includes(roomId)) {
-          ws.send(message);
+        if (oneUserSubscribeTo(parsedMessage.room)) {
+          subscribeClient.subscribe(parsedMessage.room, (message) => {
+            const parsedMessage = JSON.parse(message);
+            Object.values(subscriptions).forEach(({ ws, rooms }) => {
+              if (rooms.includes(parsedMessage.roomId)) {
+                ws.send(message);
+              }
+            });
+          });
         }
-      });
-    }
+      }
+
+      if (parsedMessage.type === "UNSUBSCRIBE") {
+        subscriptions[id].rooms = subscriptions[id].rooms.filter(
+          (x) => x !== parsedMessage.room
+        );
+      }
+
+      if (parsedMessage.type === "sendMessage") {
+        const message = parsedMessage.message;
+        const roomId = parsedMessage.roomId;
+
+        // you can only send strings
+        publishClient.publish(
+          roomId,
+          JSON.stringify({
+            type: "sendMessage",
+            roomId: roomId,
+            message,
+          })
+        );
+      }
+    });
+
+    ws.send("something");
   });
 
-  ws.send("something");
-});
-
-function randomId() {
-  return Math.random();
+  function randomId() {
+    return Math.random();
+  }
 }
+
+main();
