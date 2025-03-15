59ee870471bb7cecb3985862f38951a74e044390
Lakshay Vaishnav <thelakshayvaishnav@gmail.com>
2025-03-16 02:32:28 +0530

    added room logic

--- week-31-scalable-sockets/chat-app/dist/index.js
+++ week-31-scalable-sockets/chat-app/dist/index.js
@@ -2,11 +2,34 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
+const subscriptions = {};
+setInterval(() => {
+    console.log(subscriptions);
+}, 5000);
 wss.on("connection", function connection(ws) {
-    ws.on("error", console.error);
+    const id = randomId();
+    subscriptions[id] = {
+        ws: ws,
+        rooms: [],
+    };
     ws.on("message", function message(data) {
-        console.log("recieved data : ", data);
-        ws.send("data transferred : " + data);
+        // @ts-ignore
+        const parsedMessage = JSON.parse(data);
+        if (parsedMessage.type === "SUBSCRIBE") {
+            subscriptions[id].rooms.push(parsedMessage.room);
+        }
+        if (parsedMessage.type === "sendMessage") {
+            const message = parsedMessage.message;
+            const roomId = parsedMessage.roomId;
+            Object.values(subscriptions).forEach(({ ws, rooms }) => {
+                if (rooms.includes(roomId)) {
+                    ws.send(message);
+                }
+            });
+        }
     });
     ws.send("something");
 });
+function randomId() {
+    return Math.random();
+}
--- week-31-scalable-sockets/chat-app/src/index.ts
+++ week-31-scalable-sockets/chat-app/src/index.ts
@@ -4,6 +4,10 @@ const wss = new WebSocketServer({ port: 8080 });
 
 const subscriptions: { [key: string]: { ws: WebSocket; rooms: string[] } } = {};
 
+setInterval(() => {
+  console.log(subscriptions);
+}, 5000);
+
 wss.on("connection", function connection(ws) {
   const id = randomId();
   subscriptions[id] = {
@@ -18,12 +22,22 @@ wss.on("connection", function connection(ws) {
       subscriptions[id].rooms.push(parsedMessage.room);
     }
 
-    if (parsedMessage.type === "sendMessage") {
-      const message = parsedMessage.message;
-      
+    if (parsedMessage.type === "UNSUBSCRIBE") {
+      subscriptions[id].rooms = subscriptions[id].rooms.filter(
+        (x) => x !== parsedMessage.room
+      );
     }
 
-    ws.send("data transferred : " + data);
+    if (parsedMessage.type === "sendMessage") {
+      const message = parsedMessage.message;
+      const roomId = parsedMessage.roomId;
+
+      Object.values(subscriptions).forEach(({ ws, rooms }) => {
+        if (rooms.includes(roomId)) {
+          ws.send(message);
+        }
+      });
+    }
   });
 
   ws.send("something");
