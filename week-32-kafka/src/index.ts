b4bc1bb4777b5eaf4b3d4004272e63bd120a4f11
Lakshay Vaishnav <thelakshayvaishnav@gmail.com>
2025-04-01 09:52:48 +0530

    created basic pub sub stream in kafka

--- week-32-kafka/src/index.ts
+++ week-32-kafka/src/index.ts
@@ -6,9 +6,36 @@ const kafka = new Kafka({
 });
 
 const producer = kafka.producer();
-const consumer = kafka.consumer({groupId:"test-group"});
+const consumer = kafka.consumer({ groupId: "test-group" });
 
-const run = async() => {
-    // producing
-    
-}
+const run = async () => {
+  // producing
+  await producer.connect();
+  await producer.send({
+    topic: "quickstart-events",
+    messages: [
+      {
+        value: "hello from kafkajs producer",
+      },
+    ],
+  });
+
+  // consuming
+
+  await consumer.connect();
+  await consumer.subscribe({ topic: "quickstart-events", fromBeginning: true });
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      if (!message.value) {
+        return;
+      }
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value?.toString(),
+      });
+    },
+  });
+};
+
+run().catch(console.error);
