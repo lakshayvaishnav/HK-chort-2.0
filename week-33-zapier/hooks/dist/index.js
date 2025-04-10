06517bfe86f2445e5ca22f5351d97af1a0319b98
Lakshay Vaishnav <thelakshayvaishnav@gmail.com>
2025-04-10 13:48:27 +0530

    added basic server

---
+++ week-33-zapier/hooks/dist/index.js
@@ -0,0 +1,40 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const prisma_1 = require("../dist/generated/prisma");
+const Client = new prisma_1.PrismaClient();
+const app = (0, express_1.default)();
+app.post("/hooks/catch/:userId/:zapId", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body;
+    // store in db a new trigger
+    yield Client.$transaction((tx) => __awaiter(void 0, void 0, void 0, function* () {
+        const run = yield Client.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body,
+            },
+        });
+        yield Client.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id,
+            },
+        });
+    }));
+}));
+app.listen(3000, () => {
+    console.log("listening at 3000");
+});
--- week-33-zapier/hooks/src/index.ts
+++ week-33-zapier/hooks/src/index.ts
@@ -1,6 +1,33 @@
-import express from "express"
+import express from "express";
+import { PrismaClient } from "../dist/generated/prisma";
+
+const Client = new PrismaClient();
 
 const app = express();
 
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+  const body = req.body;
 
-app.post("")
+  // store in db a new trigger
+  await Client.$transaction(async (tx) => {
+    const run = await Client.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: body,
+      },
+    });
+
+    await Client.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+  });
+  res.json({ message: "webhook recieved" });
+});
+
+app.listen(3000, () => {
+  console.log("listening at 3000");
+});
