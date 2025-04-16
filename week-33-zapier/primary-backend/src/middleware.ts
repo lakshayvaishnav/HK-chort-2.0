01964c9197a1e5d63902eb6593cc0de358b00011
Lakshay Vaishnav <thelakshayvaishnav@gmail.com>
2025-04-16 15:16:57 +0530

    added middleware

--- week-33-zapier/primary-backend/src/middleware.ts
+++ week-33-zapier/primary-backend/src/middleware.ts
@@ -1,5 +1,20 @@
 import { NextFunction, Request, Response } from "express";
-
-export function authMiddleware  (req:Request, res:Response, next:NextFunction) {
-
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "./config";
+export function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.headers.authorization as unknown as string;
+  try {
+    const payload = jwt.verify(token, JWT_PASSWORD);
+    // @ts-ignore
+    req.id = payload.id;
+    next();
+  } catch (error) {
+    return res.status(403).json({
+      message: "you are not logged in",
+    });
+  }
 }
